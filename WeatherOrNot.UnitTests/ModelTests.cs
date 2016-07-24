baa4ee09c75e7376b6d7aefa96266a0d3377e8ec
Michael L Perry <michael@qedcode.com>
2016-07-23 22:18:02 -0500

    Added test project.

---
+++ WeatherOrNot.UnitTests/ModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeatherOrNot.UnitTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
